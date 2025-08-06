36cb247a968a8f2f0247e9060a55777c846225d7
Guettaf temam Ahmed Walid <walidguettaftemmam@gmail.com>
2025-08-06 07:16:53 +0100

    add api

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ openNav.addEventListener("click",()=>{
 closeNav.addEventListener("click",()=>{
 openNav.click()
 })
+//Api part (for messages)
+const url="https://theccomments.onrender.com"
+function sendMessage(){
+const firstName=document.querySelector('#firstname').value
+const familyName=document.querySelector('#familyname').value
+const email=document.querySelector('#email').value
+const message=document.querySelector('#message').value
+const body={
+    first_name:firstName ,
+    family_name:familyName,
+    email:email ,
+    message:message
+}
+axios.post(`${url}/comment`,body)
+.then((res)=>{
+    console.log(res.status)
+})
+}
